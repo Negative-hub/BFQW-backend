a7c3f04d89ec9447453889b7a57f7a6d29dbaaf7
prodigall.me <and020702@gmail.com>
2025-09-19 14:04:59 +0300

    add more jobs

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ credentials: true, origin: true });
-  console.log();
+  // console.log();
   await app.listen(3000);
 }
 bootstrap();
